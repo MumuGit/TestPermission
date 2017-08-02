ac8bb38ff3b95c6b1cf6c40b7ae36b0c4c4817a2
MuQi <765879176@qq.com>
2017-08-02 17:38:27 +0800

    ghgh

---
+++ app/src/main/java/com/mu/example/testpermission/AndPermission.java
@@ -0,0 +1,35 @@
+package com.mu.example.testpermission;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.AppOpsManagerCompat;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mu on 2017/8/2.
+ */
+
+public class AndPermission {
+
+    public static boolean hasPermission(@NonNull Context context, @NonNull String... permissions) {
+        return hasPermission(context, Arrays.asList(permissions));
+    }
+    public static boolean hasPermission(@NonNull Context context, @NonNull List<String> permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return true;
+        for (String permission : permissions) {
+            String op = AppOpsManagerCompat.permissionToOp(permission);
+            if (TextUtils.isEmpty(op)) continue;
+            int result = AppOpsManagerCompat.noteProxyOp(context, op, context.getPackageName());
+            if (result == AppOpsManagerCompat.MODE_IGNORED) return false;
+            result = ContextCompat.checkSelfPermission(context, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) return false;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/mu/example/testpermission/MainActivity.java
+++ app/src/main/java/com/mu/example/testpermission/MainActivity.java
@@ -1,13 +1,77 @@
 package com.mu.example.testpermission;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.PermissionListener;
+import com.yanzhenjie.permission.Rationale;
+import com.yanzhenjie.permission.RationaleListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int REQUEST_CODE_PERMISSION_SD = 100;
+    private static final int REQUEST_CODE_SETTING = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_request_single).setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_request_single:
+                AndPermission.with(this).permission(Manifest.permission.CAMERA).
+                        requestCode(REQUEST_CODE_PERMISSION_SD).
+                        callback(permissionListener).
+                        rationale(new RationaleListener() {
+                            @Override
+                            public void showRequestPermissionRationale(int requestCode, Rationale rationale) {
+                                // 这里的对话框可以自定义，只要调用rationale.resume()就可以继续申请。
+                                AndPermission.rationaleDialog(MainActivity.this, rationale).
+                                        show();
+                            }
+                        }).start();
+                break;
+        }
+    }
+
+    PermissionListener permissionListener = new PermissionListener() {
+        @Override
+        public void onSucceed(int requestCode, @NonNull List<String> grantPermissions) {
+            switch (requestCode) {
+                case REQUEST_CODE_PERMISSION_SD: {
+                    Toast.makeText(MainActivity.this, "获取日历权限成功", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onFailed(int requestCode, @NonNull List<String> deniedPermissions) {
+            switch (requestCode) {
+                case REQUEST_CODE_PERMISSION_SD: {
+                    Toast.makeText(MainActivity.this, "获取日历权限失败", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            }
+
+            // 用户否勾选了不再提示并且拒绝了权限，那么提示用户到设置中授权。
+            if (AndPermission.hasAlwaysDeniedPermission(MainActivity.this, deniedPermissions)) {
+                // 第一种：用默认的提示语。
+                AndPermission.defaultSettingDialog(MainActivity.this, 100).show();
+
+            }
+
+        }
+    };
 }
